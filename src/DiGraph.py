dd1f087abd9227622e4acdd75bc3a9e84729d193
AmirGill <amirg00@walla.com>
2021-12-20 14:05:03 +0200

    DiGraph.py has implemented, without documentations.

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,81 +1,96 @@
+# ----------------------------------------------------------------------------
+# Title:  Directed Weighted Graph - Python
+# Author: Amir Gillette, Gal Koaz
+# Course: OOP
+# ----------------------------------------------------------------------------
+
 import GraphInterface
 
 
 class DiGraph(GraphInterface):
+    def __init__(self):
+        self.node_size = 0
+        self.edge_size = 0
+        self.MC = 0
+        self.vertices = {}
+        # A dictionary of dictionary for the edges.
+        self.inEdges = {}
+        self.outEdges = {}
 
     def v_size(self):
-        """
-        Returns the number of vertices in this graph
-        @return: The number of vertices in this graph
-        """
-        raise NotImplementedError
+        return self.node_size
 
     def e_size(self):
-        """
-        Returns the number of edges in this graph
-        @return: The number of edges in this graph
-        """
-        raise NotImplementedError
+        return self.edge_size
 
     def get_all_v(self):
-        """return a dictionary of all the nodes in the Graph, each node is represented using a pair
-         (node_id, node_data)
-        """
+        return self.vertices
 
     def all_in_edges_of_node(self, id1):
-        """return a dictionary of all the nodes connected to (into) node_id ,
+        """
+
+        :param: id1:
+
+        :return: return a dictionary of all the nodes connected to (into) node_id ,
         each node is represented using a pair (other_node_id, weight)
-         """
+        """
+        return self.outEdges.get(id1)
 
     def all_out_edges_of_node(self, id1):
-        """return a dictionary of all the nodes connected from node_id , each node is represented using a pair
-        (other_node_id, weight)
-        """
+        return self.inEdges.get(id1)
 
     def get_mc(self):
-        """
-        Returns the current version of this graph,
-        on every change in the graph state - the MC should be increased
-        @return: The current version of this graph.
-        """
-        raise NotImplementedError
+        return self.MC
 
     def add_edge(self, id1, id2, weight):
-        """
-        Adds an edge to the graph.
-        @param id1: The start node of the edge
-        @param id2: The end node of the edge
-        @param weight: The weight of the edge
-        @return: True if the edge was added successfully, False o.w.
-        Note: If the edge already exists or one of the nodes dose not exists the functions will do nothing
-        """
-        raise NotImplementedError
+        # if one of the nodes doesn't exist, then the method does nothing.
+        if id1 not in self.vertices.keys() or id2 not in self.vertices.keys():
+            return
+
+        if id1 in self.inEdges.keys() and id2 in self.inEdges.get(id1):
+            return
+
+        if id1 in self.inEdges.keys():
+            self.inEdges[id1][id2] = weight
+        else:
+            self.inEdges[id1] = {}
+            self.inEdges[id1][id2] = weight
+
+        if id2 in self.outEdges.keys():
+            self.outEdges[id2][id1] = weight
+        else:
+            self.outEdges[id2] = {}
+            self.outEdges[id2][id1] = weight
+
+        self.edge_size += 1
+        self.MC += 1
 
     def add_node(self, node_id, pos):
-        """
-        Adds a node to the graph.
-        @param node_id: The node ID
-        @param pos: The position of the node
-        @return: True if the node was added successfully, False o.w.
-        Note: if the node id already exists the node will not be added
-        """
-        raise NotImplementedError
+        if node_id not in self.vertices.keys():
+            self.vertices[node_id] = pos
+            self.MC += 1
+            self.node_size += 1
+            return True
+        else:
+            return False
 
     def remove_node(self, node_id):
-        """
-        Removes a node from the graph.
-        @param node_id: The node ID
-        @return: True if the node was removed successfully, False o.w.
-        Note: if the node id does not exists the function will do nothing
-        """
-        raise NotImplementedError
+        if node_id not in self.vertices.keys():
+            return False
+        else:
+            self.vertices.pop(node_id)
+            if node_id in self.inEdges:
+                for key in self.inEdges[node_id].keys():
+                    self.remove_edge(node_id, key)
+            self.node_size -= 1
+            self.MC += 1
+            return True
 
     def remove_edge(self, node_id1, node_id2):
-        """
-        Removes an edge from the graph.
-        @param node_id1: The start node of the edge
-        @param node_id2: The end node of the edge
-        @return: True if the edge was removed successfully, False o.w.
-        Note: If such an edge does not exists the function will do nothing
-        """
-        raise NotImplementedError
+        if node_id1 in self.inEdges.keys() and node_id2 in self.inEdges[node_id1].keys():
+            self.inEdges[node_id1].pop(node_id2)
+            self.outEdges[node_id2].pop(node_id1)
+            self.MC += 1
+            self.edge_size -= 1
+        else:
+            return False
