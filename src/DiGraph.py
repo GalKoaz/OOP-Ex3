8e8fdc919736a086991c0b62aa4220f198b30217
AmirGill <amirg00@walla.com>
2021-12-20 17:11:57 +0200

    Fixed DiGraph.py and updated a few tests in DiGraph_Test.py.

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -4,30 +4,30 @@
 # Course: OOP
 # ----------------------------------------------------------------------------
 
-import GraphInterface
+from GraphInterface import GraphInterface
 
 
 class DiGraph(GraphInterface):
 
     def __init__(self):
-        self.node_size = 0
-        self.edge_size = 0
-        self.MC = 0
+        self.__node_size = 0
+        self.__edge_size = 0
+        self.__MC = 0
         self.vertices = {}
         # A dictionary of dictionary for the edges.
         self.inEdges = {}
         self.outEdges = {}
 
-    def v_size(self):
-        return self.node_size
+    def v_size(self) -> int:
+        return self.__node_size
 
-    def e_size(self):
-        return self.edge_size
+    def e_size(self) -> int:
+        return self.__edge_size
 
-    def get_all_v(self):
+    def get_all_v(self) -> dict:
         return self.vertices
 
-    def all_in_edges_of_node(self, id1):
+    def all_in_edges_of_node(self, id1: int) -> dict:
         """
 
         :param: id1:
@@ -37,19 +37,24 @@ class DiGraph(GraphInterface):
         """
         return self.outEdges.get(id1)
 
-    def all_out_edges_of_node(self, id1):
+    def all_out_edges_of_node(self, id1: int) -> dict:
         return self.inEdges.get(id1)
 
-    def get_mc(self):
-        return self.MC
+    def get_mc(self) -> int:
+        return self.__MC
 
-    def add_edge(self, id1, id2, weight):
+    def add_edge(self, id1: int, id2: int, weight: float) -> bool:
+        # if the indices are equivalent, then it couldn't be an edge.
+        if id1 == id2:
+            return False
+
         # if one of the nodes doesn't exist, then the method does nothing.
         if id1 not in self.vertices.keys() or id2 not in self.vertices.keys():
-            return
+            return False
 
+        # if there is already an edge exist we pass (meaning the method does nothing -> RETURN FALSE).
         if id1 in self.inEdges.keys() and id2 in self.inEdges.get(id1):
-            return
+            return False
 
         if id1 in self.inEdges.keys():
             self.inEdges[id1][id2] = weight
@@ -63,19 +68,20 @@ class DiGraph(GraphInterface):
             self.outEdges[id2] = {}
             self.outEdges[id2][id1] = weight
 
-        self.edge_size += 1
-        self.MC += 1
+        self.__edge_size += 1
+        self.__MC += 1
+        return True
 
-    def add_node(self, node_id, pos):
+    def add_node(self, node_id: int, pos: tuple = None) -> bool:
         if node_id not in self.vertices.keys():
             self.vertices[node_id] = pos
-            self.MC += 1
-            self.node_size += 1
+            self.__MC += 1
+            self.__node_size += 1
             return True
         else:
             return False
 
-    def remove_node(self, node_id):
+    def remove_node(self, node_id: int) -> bool:
         if node_id not in self.vertices.keys():
             return False
         else:
@@ -83,15 +89,15 @@ class DiGraph(GraphInterface):
             if node_id in self.inEdges:
                 for key in self.inEdges[node_id].keys():
                     self.remove_edge(node_id, key)
-            self.node_size -= 1
-            self.MC += 1
+            self.__node_size -= 1
+            self.__MC += 1
             return True
 
-    def remove_edge(self, node_id1, node_id2):
+    def remove_edge(self, node_id1: int, node_id2: int) -> bool:
         if node_id1 in self.inEdges.keys() and node_id2 in self.inEdges[node_id1].keys():
             self.inEdges[node_id1].pop(node_id2)
             self.outEdges[node_id2].pop(node_id1)
-            self.MC += 1
-            self.edge_size -= 1
+            self.__MC += 1
+            self.__edge_size -= 1
         else:
             return False
--- tests/DiGraph_Test.py
+++ tests/DiGraph_Test.py
@@ -6,7 +6,6 @@
 
 import unittest
 from unittest import TestCase
-from GraphInterface import DiGrap
 from DiGraph import DiGraph
 
 
@@ -15,14 +14,14 @@ class DiGraphTest(TestCase):
        Tests for class: DiGraph
     """
 
-    def setGraph(self) -> None:
+    def setUp(self) -> None:
         self.G1 = DiGraph()
-        self.G1.add_node(0, "3.5151,3.515435451,0.0")
-        self.G1.add_node(1, "3.531351,3.515435451,0.0")
-        self.G1.add_node(2, "3.714351,3.515435451,0.0")
-        self.G1.add_node(3, "3.786351,3.31465651,0.0")
-        self.G1.add_node(4, "3.62243,3.36212352353,0.0")
-        self.G1.add_node(5, "3.261674,3.563634232,0.0")
+        self.G1.add_node(0, (3.5151, 3.515435451, 0.0))
+        self.G1.add_node(1, (3.531351, 3.515435451, 0.0))
+        self.G1.add_node(2, (3.714351, 3.515435451, 0.0))
+        self.G1.add_node(3, (3.786351, 3.31465651, 0.0))
+        self.G1.add_node(4, (3.62243, 3.36212352353, 0.0))
+        self.G1.add_node(5, (3.261674, 3.563634232, 0.0))
         self.G1.add_edge(0, 1, 1.2541625654)
         self.G1.add_edge(1, 0, 1.2541625654)
         self.G1.add_edge(2, 5, 1.2541625654)
@@ -34,16 +33,40 @@ class DiGraphTest(TestCase):
         self.G1.add_edge(4, 3, 1.2541625654)
 
     def test_v_size(self):
-
         self.assertEqual(self.G1.v_size(), 6)
 
     def test_e_size(self):
         self.assertEqual(self.G1.e_size(), 9)
+        self.assertEqual(self.G1.get_mc(), 15)
+
+        self.G1.add_edge(5, 2, 1.21451)
+        self.assertEqual(self.G1.e_size(), 10)
+        self.assertEqual(self.G1.get_mc(), 16)
+
+        self.G1.add_edge(5, 2, 1.21451)
+
+        self.G1.add_edge()
+
+        self.G1.add_edge()
+
+
+        self.G1.remove_edge()
+
+        self.G1.remove_edge()
+
+        self.G1.remove_edge()
 
         self.assertEqual(True, True)
 
     def test_get_all_v(self):
-        self.assertEqual(True, True)
+        V = self.G1.get_all_v()
+        V_compare = {0: (3.5151, 3.515435451, 0.0),
+                     1: (3.531351, 3.515435451, 0.0),
+                     2: (3.714351, 3.515435451, 0.0),
+                     3: (3.786351, 3.31465651, 0.0),
+                     4: (3.62243, 3.36212352353, 0.0),
+                     5: (3.261674, 3.563634232, 0.0)}
+        self.assertEqual(V, V_compare)
 
     def test_all_in_edges_of_node(self):
         self.assertEqual(True, True)
@@ -55,9 +78,15 @@ class DiGraphTest(TestCase):
         self.assertEqual(True, True)
 
     def test_add_edge(self):
+        self.assertEqual(self.G1.get_mc(), 15)
         self.G1.add_edge(0, 4, 3.65846514)
-
-        self.assertEqual(True, True)
+        self.assertEqual(self.G1.e_size(), 10)  # Adding the same edge to see if size changes
+        self.assertEqual(self.G1.get_mc(), 16)
+        self.G1.add_edge(0, 4, 3.21541)
+        self.assertEqual(self.G1.e_size(), 10)
+        self.assertEqual(self.G1.get_mc(), 16)
+        self.assertIsNotNone(self.G1.all_in_edges_of_node(4).get(0))
+        self.assertIsNotNone(self.G1.all_out_edges_of_node(0).get(4))
 
     def test_add_node(self):
         self.assertEqual(True, True)
